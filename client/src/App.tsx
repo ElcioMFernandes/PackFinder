8cd16ed4621a608bbd3137b911d8ce4465cd76b0
Élcio M. Fernandes <elcio@elcio.dev>
2025-07-30 23:01:39 -0300

    feat: implement API call to fetch tracking data and display it

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,37 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export const App = () => {
-  return <div></div>;
+  const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    const fetch = async (): Promise<void> => {
+      try {
+        const response = await axios.post("http://localhost:3001/api/track", {
+          code: "BR123456789BR",
+        });
+
+        if (response.status >= 200 && response.status < 300) {
+          setData(response.data);
+        } else {
+          console.error(`
+            API request failed with status ${response.status}
+            Response: ${JSON.stringify(response.data)}
+           `);
+        }
+      } catch (error) {
+        const message =
+          error instanceof Error ? error.message : "An unknown error occurred";
+        throw new Error(`API request failed: ${message}`);
+      }
+    };
+
+    fetch();
+  }, []);
+
+  return (
+    <div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 };
