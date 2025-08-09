4c7a8e238cbeba3825e61bd4edea835a31f64cb1
Élcio M. Fernandes <elcio@elcio.dev>
2025-08-08 23:30:44 -0300

    feat: refactor App component to use Routes for page navigation and protect routes with authentication

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,51 +1,70 @@
+import { Protected } from "./security";
+import { Routes, Route } from "react-router-dom";
 import {
-  Sidebar,
-  SidebarItem,
-  SidebarHeader,
-  SidebarFooter,
-  SidebarContent,
-} from "./components";
-import useSWR from "swr";
-import { useState } from "react";
-import { usePage } from "./hooks";
-import { fetcher } from "./services";
-import { pagesConfig } from "./pages";
-import type { Page } from "./types";
+  About,
+  Access,
+  Contact,
+  History,
+  Features,
+  Monition,
+  Tracking,
+} from "./pages";
 
 export const App = () => {
-  const { currentPage, navigate } = usePage();
-  const [trackingCode, setTrackingCode] = useState<string>("AC887698372BR");
-
-  const { data, error, isLoading } = useSWR(
-    trackingCode ? ["http://localhost:3001/api/track", trackingCode] : null,
-    ([url, code]) => fetcher(url, code),
-    {
-      revalidateOnFocus: false,
-      revalidateOnReconnect: false,
-    }
-  );
-
-  const CurrentPageComponent = pagesConfig[currentPage].component;
-
   return (
     <div className="w-screen h-screen flex flex-row overflow-hidden bg-neutral-100 text-neutral-950 dark:bg-neutral-950 dark:text-neutral-100">
-      <Sidebar>
-        <SidebarHeader>PackFinder.io</SidebarHeader>
-        <SidebarContent>
-          {Object.entries(pagesConfig).map(([pageKey, config]) => (
-            <SidebarItem
-              key={pageKey}
-              onClick={() => navigate(pageKey as Page)}
-            >
-              {config.icon} {config.label}
-            </SidebarItem>
-          ))}
-        </SidebarContent>
-        <SidebarFooter>Versão 1.0.0</SidebarFooter>
-      </Sidebar>
-
       <main className="flex-1 overflow-y-auto">
-        <CurrentPageComponent />
+        <Routes>
+          <Route path="/" element={<Access />} />
+          <Route
+            path="/about"
+            element={
+              <Protected>
+                <About />
+              </Protected>
+            }
+          />
+          <Route
+            path="/contact"
+            element={
+              <Protected>
+                <Contact />
+              </Protected>
+            }
+          />
+          <Route
+            path="/history"
+            element={
+              <Protected>
+                <History />
+              </Protected>
+            }
+          />
+          <Route
+            path="/tracking"
+            element={
+              <Protected>
+                <Tracking />
+              </Protected>
+            }
+          />
+          <Route
+            path="/monition"
+            element={
+              <Protected>
+                <Monition />
+              </Protected>
+            }
+          />
+          <Route
+            path="/features"
+            element={
+              <Protected>
+                <Features />
+              </Protected>
+            }
+          />
+        </Routes>
       </main>
     </div>
   );
