0607a0ab447f060a7dc8375d515bb4ff681bc9a8
Élcio M. Fernandes <elcio@elcio.dev>
2025-08-08 23:29:18 -0300

    refactor: simplify Sidebar component and update sidebar styles

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,12 +1,6 @@
 import { cn } from "../utils";
 import { forwardRef } from "react";
-import {
-  sidebar,
-  sidebarItem,
-  sidebarHeader,
-  sidebarFooter,
-  sidebarContent,
-} from "../styles";
+import { sidebar } from "../styles";
 
 import type { ComponentProps } from "react";
 import type { VariantProps } from "class-variance-authority";
@@ -25,86 +19,6 @@ export const Sidebar = forwardRef<HTMLElement, Props>(
       >
         {children}
       </nav>
-    );
-  }
-);
-
-interface SidebarHeaderProps
-  extends ComponentProps<"div">,
-    VariantProps<typeof sidebarHeader> {
-  asChild?: boolean;
-}
-
-export const SidebarHeader = forwardRef<HTMLDivElement, SidebarHeaderProps>(
-  ({ className, variant, size, asChild = false, children, ...props }, ref) => {
-    return (
-      <div
-        className={cn(sidebarHeader({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      >
-        {children}
-      </div>
-    );
-  }
-);
-
-interface SidebarContentProps
-  extends ComponentProps<"div">,
-    VariantProps<typeof sidebarContent> {
-  asChild?: boolean;
-}
-
-export const SidebarContent = forwardRef<HTMLDivElement, SidebarContentProps>(
-  ({ className, variant, size, asChild = false, children, ...props }, ref) => {
-    return (
-      <div
-        className={cn(sidebarContent({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      >
-        {children}
-      </div>
-    );
-  }
-);
-
-interface SidebarItemProps
-  extends ComponentProps<"div">,
-    VariantProps<typeof sidebarItem> {
-  asChild?: boolean;
-}
-
-export const SidebarItem = forwardRef<HTMLDivElement, SidebarItemProps>(
-  ({ className, variant, size, asChild = false, children, ...props }, ref) => {
-    return (
-      <div
-        className={cn(sidebarItem({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      >
-        {children}
-      </div>
-    );
-  }
-);
-
-interface SidebarFooterProps
-  extends ComponentProps<"div">,
-    VariantProps<typeof sidebarFooter> {
-  asChild?: boolean;
-}
-
-export const SidebarFooter = forwardRef<HTMLDivElement, SidebarFooterProps>(
-  ({ className, variant, size, asChild = false, children, ...props }, ref) => {
-    return (
-      <div
-        className={cn(sidebarFooter({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      >
-        {children}
-      </div>
     );
   }
 );
--- client/src/styles/sidebar.ts
+++ client/src/styles/sidebar.ts
@@ -1,91 +1,19 @@
 import { cva } from "class-variance-authority";
 
-export const sidebar = cva("flex flex-col p-2 bg-neutral-900", {
+export const sidebar = cva("flex flex-col p-4 gap-2 bg-neutral-900", {
   variants: {
     variant: {
-      primary: "",
-      secondary: "",
+      pri: "",
+      sec: "",
     },
     size: {
-      sm: "",
-      md: "",
-      lg: "",
+      sm: "min-w-30",
+      md: "min-w-64",
+      lg: "min-w-96",
     },
   },
   defaultVariants: {
-    variant: "primary",
-    size: "md",
-  },
-});
-
-export const sidebarHeader = cva("flex items-center gap-3", {
-  variants: {
-    variant: {
-      primary: "",
-      secondary: "",
-    },
-    size: {
-      sm: "",
-      md: "",
-      lg: "",
-    },
-  },
-  defaultVariants: {
-    variant: "primary",
-    size: "md",
-  },
-});
-
-export const sidebarContent = cva("flex-1 overflow-y-auto", {
-  variants: {
-    variant: {
-      primary: "",
-      secondary: "",
-    },
-    size: {
-      sm: "",
-      md: "",
-      lg: "",
-    },
-  },
-  defaultVariants: {
-    variant: "primary",
-    size: "md",
-  },
-});
-
-export const sidebarItem = cva("flex items-center gap-2 hover:bg-neutral-600", {
-  variants: {
-    variant: {
-      primary: "",
-      secondary: "",
-    },
-    size: {
-      sm: "",
-      md: "",
-      lg: "",
-    },
-  },
-  defaultVariants: {
-    variant: "primary",
-    size: "md",
-  },
-});
-
-export const sidebarFooter = cva("flex items-center gap-3", {
-  variants: {
-    variant: {
-      primary: "",
-      secondary: "",
-    },
-    size: {
-      sm: "",
-      md: "",
-      lg: "",
-    },
-  },
-  defaultVariants: {
-    variant: "primary",
+    variant: "pri",
     size: "md",
   },
 });
